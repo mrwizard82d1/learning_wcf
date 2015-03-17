fc2629f2480034c3979314d18c05a363b98f5a40
Larry Jones <mrwizard82d1@gmail.com>
2015-03-17 08:22:30 -0500

    Added (console) service host.
    This service runs (and terminates). In addition, if you navigate to the
    endpoint from a browser, you get a message stating that this endpoint
    **does not** expose service metadata; however, the page also correctly
    indicates that you have navigated to a WCF service.

---
+++ labs/ch1/ServiceFromScratch/Host/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ServiceModel;
+using HelloIndigo;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (
+                var host = new ServiceHost(typeof (HelloIndigoService),
+                    new Uri("http://localhost:8000/HelloIndigo")))
+            {
+                host.AddServiceEndpoint(typeof (IHelloIndigoService),
+                    new BasicHttpBinding(), "HelloIndigoService");
+                host.Open();
+
+                Console.WriteLine(
+                    "Press <ENTER> to terminate the service host.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
