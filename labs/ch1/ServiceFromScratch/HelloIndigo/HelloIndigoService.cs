50c392679c0f81d50ea8b88c682ea4898214eae2
Larry Jones <mrwizard82d1@gmail.com>
2015-03-17 08:16:31 -0500

    Created service.

---
+++ labs/ch1/ServiceFromScratch/HelloIndigo/HelloIndigoService.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceModel;
+
+namespace HelloIndigo
+{
+    /// <summary>
+    /// An interface specifying the contract for this service.
+    /// </summary>
+    [ServiceContract(Name="http://www.thatindigogirl.com/samples/2006/06")]
+    public interface IHelloIndigoService
+    {
+        [OperationContract]
+        string HelloIndigo();
+    }
+
+    public class HelloIndigoService : IHelloIndigoService
+    {
+        public string HelloIndigo()
+        {
+            return "Hello, Indigo!";
+        }
+    }
+}
