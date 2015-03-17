2216f414814644534c2b81fbc1ff3c0812f16398
Larry Jones <mrwizard82d1@gmail.com>
2015-03-17 08:33:34 -0500

    Created client.
    Again, the client and the server work. I configured Visual Studio to
    start **both** projects. (Select solution in `Solution Explorer`. Click
    `Project > Properties`. Select `Startup Project` in tree. Check
    `Multiple startup projects`. Change actions of `Host` and `Client` to
    `Start`. Use up and down errors to start `Host` **first** and to then
    start `Client`.

---
+++ labs/ch1/ServiceFromScratch/Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Client
+{
+    /// <summary>
+    /// An **copy** of the interface specifying the contract for this service.
+    /// </summary>
+    [ServiceContract(Name = "http://www.thatindigogirl.com/samples/2006/06")]
+    public interface IHelloIndigoService
+    {
+        [OperationContract]
+        string HelloIndigo();
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var endPointAddress =
+                new EndpointAddress(
+                    "http://localhost:8000/HelloIndigo/HelloIndigoService");
+            var proxy =
+                ChannelFactory<IHelloIndigoService>.CreateChannel(
+                    new BasicHttpBinding(), endPointAddress);
+            var result = proxy.HelloIndigo();
+            Console.WriteLine(result);
+
+            Console.WriteLine("Press <ENTER> to terminate client.");
+            Console.ReadLine();
+        }
+    }
+}
