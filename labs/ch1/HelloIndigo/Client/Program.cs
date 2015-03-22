5385061e2eeb9dd190a7129727189e717499d994
Larry Jones <mrwizard82d1@gmail.com>
2015-03-22 18:33:46 -0500

    Create client service reference. Client works!
    Add a service reference to the `Client` project following the
    instructions in the book. Build all projects. Set startup project to
    include both `Host` and `Client` if not already done. Start all
    projects.
    
    The one configuration detail that I noticed was that the
    `bindingConfiguration` attribute was "basicHttp" - **not** an empty
    string. I'll go back and change that in the `unit_test_broke` branch to
    see if the unit tests now pass.

--- labs/ch1/HelloIndigo/Client/Program.cs
+++ labs/ch1/HelloIndigo/Client/Program.cs
@@ -6,6 +6,11 @@ namespace Client
     {
         static void Main()
         {
+            var proxy = new localhost.HelloIndigoServiceClient();
+            var result = proxy.HelloIndigo();
+            Console.WriteLine(result);
+            Console.WriteLine();
+
             Console.WriteLine("Press <ENTER> to terminate client.");
             Console.ReadLine();
         }
