011896374baf466cce42239400a2af5e65a1327f
Larry Jones <mrwizard82d1@gmail.com>
2015-03-21 23:09:11 -0500

    Create skeleton (console) host project.
    This skeleton is an empty console project that simply waits for the user
    to press the <ENTER> key and then terminates.
    
    Later, I will add to the code to create an actual service host.
    
    Because it is a skeleton, I can actually copy the `Host` project to
    create a similar `Client` project.

---
+++ labs/ch1/HelloIndigo/Host/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Press <ENTER> to terminate host.");
+            Console.ReadLine();
+        }
+    }
+}
