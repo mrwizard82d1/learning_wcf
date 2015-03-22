c51d8db479bb90d9241a45bc6bc8d80e1349b200
Larry Jones <mrwizard82d1@gmail.com>
2015-03-22 14:26:47 -0500

    Add spec test for service host (failing).
    Add a .feature file describing how to test the service host. Implement
    the test steps generated for that .feature file. When run the test fails
    with the following error:
    
    error: There was no endpoint listening at http://localhost:8000/HelloIndigo/HelloIndigoService that could accept the message. This is often caused by an incorrect address or SOAP action. See InnerException, if present, for more details.
    
    I believe this error results from different configuration for the
    endpoint. Since I copied the configuration from the `ServiceFromScratch`
    solution, I suspect I have something incorrect: perhaps spelling or a
    different port number.
    
    I decided to check in the code and work a bit more on the steps outlined
    in the book, "Learning WCF," believing these steps will enlighten me. :)

---
+++ labs/ch1/HelloIndigo/Host.Specs/Host.feature
@@ -0,0 +1,9 @@
+﻿Feature: Run Service Host
+  In order to learn to test a hosted, running WCF service
+  As a developer
+  I want to test a running, hosted WCF service
+
+Scenario: Query greeting
+  Given that I have started the host
+  When I execute the "HelloIndigo" method of the service
+  Then I receive "Hello Indigo" as a result
---
+++ labs/ch1/HelloIndigo/Host.Specs/RunServiceHostSteps.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.ServiceModel;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Host.Specs
+{
+    /// <summary>
+    /// An **copy** of the interface specifying the contract for this service.
+    /// </summary>
+    [ServiceContract(
+        Namespace = "http://www.thatindigogirl.com/samples/2006/06")]
+    public interface IHelloIndigoService
+    {
+        [OperationContract]
+        string HelloIndigo();
+    }
+
+    [Binding]
+    public class RunServiceHostSteps
+    {
+        [Given(@"that I have started the host")]
+        public void GivenThatIHaveStartedTheHost()
+        {
+            Process.Start(@"..\..\..\Host\bin\Debug\Host.exe");
+        }
+
+        [When(@"I execute the ""(.*)"" method of the service")]
+        public void WhenIExecuteTheMethodOfTheService(string p0)
+        {
+            var endPointAddress =
+                new EndpointAddress(
+                    "http://localhost:8000/HelloIndigo/HelloIndigoService");
+            var proxy =
+                ChannelFactory<IHelloIndigoService>.CreateChannel(
+                    new BasicHttpBinding(), endPointAddress);
+            ScenarioContext.Current.Set(proxy);
+        }
+        
+        [Then(@"I receive ""(.*)"" as a result")]
+        public void ThenIReceiveAsAResult(string p0)
+        {
+            var proxy = ScenarioContext.Current.Get<IHelloIndigoService>();
+            var result = proxy.HelloIndigo();
+            Assert.That(result, Is.EqualTo("Hello Indigo"));
+        }
+    }
+}
