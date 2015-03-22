a06546bc82c6dfea59bd7ca76ead50278bec5bed
Larry Jones <mrwizard82d1@gmail.com>
2015-03-21 19:46:36 -0500

    Add comments about relative Service address.
    Make no functional change; simply added comments to clarify that
    solution uses a base address for the service, that the specific
    service is specified using a relative address, and describing the
    complete address of the service based on these two components.

--- labs/ch1/ServiceFromScratch/Host/Program.cs
+++ labs/ch1/ServiceFromScratch/Host/Program.cs
@@ -8,11 +8,19 @@ namespace Host
     {
         static void Main()
         {
+            // A side effect (although perhaps documented)  of supplying 
+            // a Uri to the ServiceHost contstructor is that this Uri
+            // becomes the base address for the service. 
             using (
                 var host = new ServiceHost(typeof (HelloIndigoService),
                     new Uri("http://localhost:8000/HelloIndigo")))
             {
-                host.AddServiceEndpoint(typeof (IHelloIndigoService),
+                // By specifying the base address for the service when
+                // constructing the ServiceHost, we can then specify 
+                // endpoints using a relative address. That is, the 
+                // complete address of the service is 
+                // `http://localhost:8080/HelloIndigo/HelloIndigoService`.
+                host.AddServiceEndpoint(typeof (HelloIndigoService),
                     new BasicHttpBinding(), "HelloIndigoService");
                 host.Open();
 
